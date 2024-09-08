5e49ce6f611314c9ab4dda0e7452e43b414ccc8d
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-09-08 15:03:06 -0700

    Ahora si ya

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ miniaturas.forEach(miniatura => {
         const tempo = principal.src;
         principal.src = this.src;
         this.src = tempo;
+
+        principal.style.objectFit = 'cover';
+        principal.style.width = '600px';
+        principal.style.height = '400px';
     });
 });
